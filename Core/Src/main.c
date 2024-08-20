51fcd2c5b8c58edc1b0685e145c0b2e861ed3f19
El-Matata <154691289+El-Matata@users.noreply.github.com>
2024-08-20 19:54:40 -0400

    Cleaned up main add increased rtt buffer

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -72,6 +72,7 @@ void SystemClock_Config(void);
   */
 int main(void)
 {
+  extern uint8_t LoRa_buff[RH_RF95_FIFO_SIZE];
 
   /* USER CODE BEGIN 1 */
   char abDataIn[RTT_PRINTF_BUFFER_SIZE];
@@ -102,47 +103,21 @@ int main(void)
   MX_I2C1_Init();
   MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
+  HAL_UART_Init(&huart2);
   SEGGER_RTT_Init();
   SEGGER_RTT_SetTerminal(0);
-
   SEGGER_RTT_ConfigDownBuffer(1, "DataIn", &abDataIn[0], sizeof(abDataIn), SEGGER_RTT_MODE_NO_BLOCK_SKIP);
   SEGGER_RTT_ConfigUpBuffer(1, "DataOut", &abDataOut[0], sizeof(abDataOut), SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
+  RF95_Init();
 
-  int NumBytes=0;
-
-  HAL_UART_Init(&huart2);
-
-  //RF95_Init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-        /*
-    if(SEGGER_RTT_HasKey()){
-      NumBytes = SEGGER_RTT_GetKey();
-      HAL_GPIO_TogglePin(LED_1_GPIO_Port,LED_1_Pin);
-      HAL_Delay(100);
-      HAL_GPIO_TogglePin(LED_1_GPIO_Port,LED_1_Pin);
-      SEGGER_RTT_printf(0,"%c",NumBytes);
-    }*/
-
-
-    a = ReceiveMsgSerial();
-    SendMsgSerial((uint8_t*)ReceiveBuffer,a);
- 
-    /*
-    if(SEGGER_RTT_HasKey()){
-      NumBytes = SEGGER_RTT_Read(0, &abDataIn[0], RTT_PRINTF_BUFFER_SIZE);
-      SEGGER_RTT_printf(0,"%s",abDataIn);
-      memset(abDataIn, '\0', sizeof(abDataIn)); 
-    }*/ 
-
-    //RF95_Master_Receive_from_All_Nodes();
-    //RF95_Slave_Send();
-    //HAL_GPIO_TogglePin(LED_1_GPIO_Port,LED_1_Pin);
-    //HAL_Delay(100);
+    HAL_GPIO_TogglePin(LED_1_GPIO_Port,LED_1_Pin);
+    HAL_Delay(100);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
